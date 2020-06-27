aadfdb280bb1d4a0384a453a36f8857026fb5e40
Pegunito <artyomsasha@mail.ru>
2020-06-27 11:14:07 +0400

    выборка обьекта

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
-const string = "Привет, как дела?";
+const workers = [
+  {"name":"John","salary":500},
+  {"name":"Mike","salary":1300},
+  {"name":"Linda","salary":1500}
+];
 
-const getVowels = stringToFilter => {
-  return stringToFilter;
+const getWorthyWorkers = (workersArr) => {
+
+  const worthyWorkers = [];
+
+  for (let i = 0; i < workersArr.lenght; i++) {
+    const currentWorker = workersArr[i];
+
+    if (currentWorker.salary > 1000) {
+      worthyWorkers.push(currentWorker);
+    }
+  }
+
+  return worthyWorkers;
 }
 
-console.log(getVowels(string));
+console.log(getWorthyWorkers(workers));
